218f7f35c5453a0a3f2cc468acc1bb4dc5f1b9fb
xyzbtw <artemoso10@gmail.com>
2023-03-08 11:44:57 -0600

    Bad Anchor little update

--- src/main/java/me/earth/earthhack/impl/modules/movement/badanchor/BadAnchor.java
+++ src/main/java/me/earth/earthhack/impl/modules/movement/badanchor/BadAnchor.java
@@ -5,9 +5,7 @@ import me.earth.earthhack.api.module.util.Category;
 import me.earth.earthhack.api.setting.Setting;
 import me.earth.earthhack.api.setting.settings.BooleanSetting;
 import me.earth.earthhack.api.setting.settings.NumberSetting;
-import net.minecraft.util.math.BlockPos;
 
-import java.util.ArrayList;
 
 public class BadAnchor extends Module {
     protected final Setting<Boolean> pull =
@@ -15,16 +13,10 @@ public class BadAnchor extends Module {
 
     protected final Setting<Integer> pitch =
             register(new NumberSetting<>("Pitch", 60, -90, 90));
-    private final ArrayList<BlockPos> holes = new ArrayList<BlockPos>();
-    public static boolean AnchorING;
-
     public BadAnchor() {
         super("LegacyAnchor", Category.Movement);
         this.listeners.add(new ListenerUpdate(this));
     }
-    @Override
-    public void onDisable() {
-        AnchorING = false;
-    }
+
 
 }
--- src/main/java/me/earth/earthhack/impl/modules/movement/badanchor/ListenerUpdate.java
+++ src/main/java/me/earth/earthhack/impl/modules/movement/badanchor/ListenerUpdate.java
@@ -2,6 +2,7 @@ package me.earth.earthhack.impl.modules.movement.badanchor;
 
 import me.earth.earthhack.impl.event.events.misc.UpdateEvent;
 import me.earth.earthhack.impl.event.listeners.ModuleListener;
+import me.earth.earthhack.impl.util.math.RayTraceUtil;
 import me.earth.earthhack.impl.util.minecraft.blocks.HoleUtil;
 import net.minecraft.util.math.Vec3d;
 
@@ -15,6 +16,9 @@ public class ListenerUpdate extends ModuleListener<BadAnchor, UpdateEvent> {
 
     @Override
     public void invoke(UpdateEvent event) {
+        double x = getPlayerPos().getX();
+        double y = getPlayerPos().getY();
+        double z = getPlayerPos().getZ();
         if (mc.world == null) {
             return;
         }
@@ -23,33 +27,27 @@ public class ListenerUpdate extends ModuleListener<BadAnchor, UpdateEvent> {
         }
         if (mc.player.rotationPitch >= module.pitch.getValue()) {
             if (
-                    HoleUtil.isHole(getPlayerPos().down(1), false)[0]
-                    || HoleUtil.isHole(getPlayerPos().down(2), false)[0]
-                    || HoleUtil.isHole(getPlayerPos().down(3), false)[0]
-                    || HoleUtil.isHole(getPlayerPos().down(4), false)[0]
+                    (HoleUtil.isHole(getPlayerPos().down(1), false)[0]
+                            && RayTraceUtil.canBeSeen(x, y-1, z, mc.player))
+                    || (HoleUtil.isHole(getPlayerPos().down(2), false)[0]
+                            && RayTraceUtil.canBeSeen(x, y-2, z, mc.player))
+                    || (HoleUtil.isHole(getPlayerPos().down(3), false)[0]
+                            && RayTraceUtil.canBeSeen(x, y-3, z, mc.player))
+                    || (HoleUtil.isHole(getPlayerPos().down(4), false)[0]
+                            && RayTraceUtil.canBeSeen(x, y-4, z, mc.player))
             )
             {
-                BadAnchor.AnchorING = true;
                 if (!module.pull.getValue()) {
                     mc.player.motionX = 0.0;
                     mc.player.motionZ = 0.0;
                 } else {
                     Vec3d center = GetCenter(mc.player.posX, mc.player.posY, mc.player.posZ);
-                    double XDiff = Math.abs(center.x - mc.player.posX);
-                    double ZDiff = Math.abs(center.z - mc.player.posZ);
-
-                    if (XDiff <= 0.1 && ZDiff <= 0.1) {
-                        center = Vec3d.ZERO;
-                    }
-                    else {
-                        double MotionX = center.x-mc.player.posX;
-                        double MotionZ = center.z-mc.player.posZ;
-
-                        mc.player.motionX = MotionX/2;
-                        mc.player.motionZ = MotionZ/2;
-                    }
+                    double MotionX = center.x-mc.player.posX;
+                    double MotionZ = center.z-mc.player.posZ;
+                    mc.player.motionX = MotionX/2;
+                    mc.player.motionZ = MotionZ/2;
                 }
-            } else BadAnchor.AnchorING = false;
+            }
         }
     }
 
