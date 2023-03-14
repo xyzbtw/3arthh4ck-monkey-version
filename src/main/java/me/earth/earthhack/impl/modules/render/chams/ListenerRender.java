cd7f340ef1e47320c7f595ff795e24f91d2fc312
xyzbtw <artemoso10@gmail.com>
2023-03-14 11:41:03 -0500

    idk why this got removed

--- src/main/java/me/earth/earthhack/impl/modules/render/chams/ListenerRender.java
+++ src/main/java/me/earth/earthhack/impl/modules/render/chams/ListenerRender.java
@@ -30,7 +30,114 @@ public class ListenerRender
     public void invoke(Render3DEvent event)
     {
         // TODO: read stenciling for army and people who can't use shaders
-        if (false)
+        if (false
+                && !ESP.isRendering
+                && !Nametag.isRendering)
+        {
+            double renderPosX = ((IRenderManager) mc.getRenderManager()).getRenderPosX();
+            double renderPosY = ((IRenderManager) mc.getRenderManager()).getRenderPosY();
+            double renderPosZ = ((IRenderManager) mc.getRenderManager()).getRenderPosZ();
+
+            module.checkSetupFBO();
+            glPushAttrib(GL_ALL_ATTRIB_BITS);
+            glClear(GL_STENCIL_BUFFER_BIT); // clear stencil buffer, we will write 1s only where the entity is being rendered
+            glClearStencil(0);
+            glEnable(GL_STENCIL_TEST); // enable stencil testing
+            glStencilOp(GL_KEEP, GL_KEEP, GL_REPLACE);
+            glStencilFunc(GL_ALWAYS, 4, 0xFF); // all fragments should pass the stencil test
+            glStencilMask(0xFF); // enable writing to the stencil buffer
+            glColor4f(1.0f, 1.0f, 1.0f, 1.0f); // color white
+            glDisable(GL_TEXTURE_2D);
+            boolean renderShadows = mc.gameSettings.entityShadows;
+            mc.gameSettings.entityShadows = false;
+            module.force = true;
+            module.renderLayers = false;
+            module.renderModels = true;
+            renderEntities(renderPosX, renderPosY, renderPosZ);
+            module.renderLayers = true;
+            module.renderModels = false;
+
+            /*glEnable(GL_ALPHA_TEST);
+            glAlphaFunc(GL_GEQUAL, 0.1f);*/
+
+            if (module.alphaShader != null)
+            {
+                // glEnable(GL_DEPTH_TEST);
+                // glDepthMask(true);
+                glEnable(GL_TEXTURE_2D);
+                // OpenGlHelper.setLightmapTextureCoords(OpenGlHelper.lightmapTexUnit, 240.0f, 240.0f);
+                module.alphaShader.bind();
+                module.alphaShader.set("sampler", 0);
+                glStencilOp(GL_KEEP, GL_KEEP, GL_REPLACE);
+                glStencilFunc(GL_ALWAYS, 3, 0xFF);
+                glStencilMask(0xFF);
+                renderEntities(renderPosX, renderPosY, renderPosZ);
+                module.alphaShader.unbind();
+            }
+
+            module.force = false;
+            mc.gameSettings.entityShadows = renderShadows;
+            // glDisable(GL_ALPHA_TEST);
+
+            // render the held stuff, but use a shader with the discord keyword to discord low alpha fragments, fixing our depth issues! genius!
+            /*glEnable(GL_TEXTURE_2D);
+            if (OpenGlHelper.areShadersSupported()
+                    && module.alphaShader != null)
+            {
+                // glEnable(GL_ALPHA_TEST);
+                // glAlphaFunc(GL_LEQUAL, 0.5f);
+                module.alphaShader.bind();
+                module.alphaShader.set("texture", 0);
+                glStencilFunc(GL_ALWAYS, 0, 0xFF);
+                renderEntities(renderPosX, renderPosY, renderPosZ);
+                module.alphaShader.unbind();
+                // glDisable(GL_ALPHA_TEST);
+            }*/
+
+            // TODO: fragment shader to discard ^^^
+
+            glStencilFunc(GL_EQUAL, 4, 0xFF); // only pass if the buffer is not equal to 0
+            glStencilMask(0x00); // no longer writing to the stencil buffer
+            // reset projection and modelview matrices
+            FloatBuffer buffer = GLAllocation.createDirectFloatBuffer(16);
+            FloatBuffer modelViewBuffer = GLAllocation.createDirectFloatBuffer(16);
+            glGetFloat(GL_PROJECTION_MATRIX, buffer);
+            glGetFloat(GL_MODELVIEW_MATRIX, modelViewBuffer);
+            ScaledResolution scaledresolution = new ScaledResolution(mc);
+            glMatrixMode(GL_PROJECTION);
+            glLoadIdentity();
+            GlStateManager.ortho(0.0D, scaledresolution.getScaledWidth_double(), scaledresolution.getScaledHeight_double(), 0.0D, 1000.0D, 3000.0D);
+            glMatrixMode(GL_MODELVIEW);
+            glLoadIdentity();
+            // GlStateManager.translate(0.0F, 0.0F, -500.0f);
+            GlStateManager.translate(0.0F, 0.0F, module.z.getValue());
+            glEnable(GL_TEXTURE_2D);
+            glDisable(GL_BLEND);
+
+            // finally draw texture
+
+            mc.getTextureManager().bindTexture(Chams.GALAXY_LOCATION);
+            glColor4f(1.0f, 1.0f, 1.0f, 1.0f);
+            // drawCompleteImage(0, 0, mc.displayWidth, mc.displayHeight);
+
+            // Render2DUtil.drawTexturedRect(0, 0, 0, 0, scaledresolution.getScaledWidth(), scaledresolution.getScaledHeight(), -500);
+
+            Gui.drawModalRectWithCustomSizedTexture(0, 0, 0, 0, scaledresolution.getScaledWidth(), scaledresolution.getScaledHeight(), scaledresolution.getScaledWidth(), scaledresolution.getScaledHeight());
+
+            glMatrixMode(GL_PROJECTION);
+            glLoadMatrix(buffer);
+            glMatrixMode(GL_MODELVIEW);
+            glDisable(GL_STENCIL_TEST);
+            glPopAttrib();
+
+            // renderEntities(renderPosX, renderPosY, renderPosZ);
+            /*if (!OpenGlHelper.areShadersSupported()
+                    || module.alphaShader == null)
+            {
+
+            }*/
+        }
+        else if (false)
         {
             if (true) return;
             if (module.imageShader != null)
