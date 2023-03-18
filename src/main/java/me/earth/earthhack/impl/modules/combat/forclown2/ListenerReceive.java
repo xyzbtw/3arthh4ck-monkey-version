2da913dcf97ec8cbc0e2680317710321cd066f6a
AquaMinerale2b2t <96651128+AquaMinerale2b2t@users.noreply.github.com>
2023-03-18 14:36:01 +0300

    cleaned up anti cev

--- src/main/java/me/earth/earthhack/impl/modules/combat/forclown2/ListenerReceive.java
+++ src/main/java/me/earth/earthhack/impl/modules/combat/forclown2/ListenerReceive.java
@@ -1,15 +1,12 @@
 package me.earth.earthhack.impl.modules.combat.forclown2;
 
-
 import me.earth.earthhack.impl.event.events.network.PacketEvent;
 import me.earth.earthhack.impl.event.listeners.ModuleListener;
-import me.earth.earthhack.impl.util.minecraft.PlayerUtil;
 import net.minecraft.init.Blocks;
 import net.minecraft.network.play.server.SPacketBlockBreakAnim;
 import net.minecraft.util.math.BlockPos;
 
 public class ListenerReceive extends ModuleListener<forclown2, PacketEvent.Receive<SPacketBlockBreakAnim>> {
-
 
     public ListenerReceive(forclown2 module) {
         super(module, PacketEvent.Receive.class);
@@ -17,24 +14,23 @@ public class ListenerReceive extends ModuleListener<forclown2, PacketEvent.Recei
 
     @Override
     public void invoke(PacketEvent.Receive<SPacketBlockBreakAnim> event) {
-            if(event.getPacket() != null && PlayerUtil.isInHole(mc.player)){
+        //hole check was redundant
+        if(event.getPacket() != null){
+            BlockPos pos = event.getPacket().getPosition();
 
-                BlockPos pos = event.getPacket().getPosition();
+            //???
+            if (mc.world.getBlockState(pos).getBlock() == (Blocks.BEDROCK) || mc.world.getBlockState(pos).getBlock() == (Blocks.AIR)) return;
 
-                if (mc.world.getBlockState(pos).getBlock() == (Blocks.BEDROCK) || mc.world.getBlockState(pos).getBlock() == (Blocks.AIR)) return;
+            BlockPos playerPos = mc.player.getPosition();
+            BlockPos placePos = null;
 
-                BlockPos playerPos = new BlockPos(mc.player.posX, mc.player.posY, mc.player.posZ);
-                BlockPos placePos = null;
-
-                if(pos.equals(playerPos.add(0,2,0))){
-                    placePos = playerPos.add(0,3,0);
-                }
-
-                if (placePos != null) {
-                    module.placeBlock(placePos);
-                }
-
-
+            if(pos.equals(playerPos.add(0,2,0))){
+                placePos = playerPos.add(0,3,0);
             }
+
+            if (placePos != null) {
+                module.placeBlock(placePos);
+            }
+        }
     }
 }
