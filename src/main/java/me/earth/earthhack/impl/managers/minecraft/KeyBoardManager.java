6802af1858d44d9b54df5575a800e053dcc8d846
xyzbtw <89705569+xyzbtw@users.noreply.github.com>
2023-06-08 00:53:26 -0600

    maybe fixed hold bug in guis

--- src/main/java/me/earth/earthhack/impl/managers/minecraft/KeyBoardManager.java
+++ src/main/java/me/earth/earthhack/impl/managers/minecraft/KeyBoardManager.java
@@ -10,58 +10,41 @@ import me.earth.earthhack.impl.managers.client.ModuleManager;
 import me.earth.earthhack.impl.util.minecraft.KeyBoardUtil;
 import me.earth.earthhack.pingbypass.PingBypass;
 
-public class KeyBoardManager extends SubscriberImpl
-{
+public class KeyBoardManager extends SubscriberImpl {
     private final ModuleManager moduleManager;
 
-    public KeyBoardManager()
-    {
+    public KeyBoardManager() {
         this(Managers.MODULES);
     }
 
-    public KeyBoardManager(ModuleManager moduleManager)
-    {
+    public KeyBoardManager(ModuleManager moduleManager) {
         this.moduleManager = moduleManager;
-        this.listeners.add(new EventListener<KeyboardEvent>(KeyboardEvent.class)
-        {
+        this.listeners.add(new EventListener<KeyboardEvent>(KeyboardEvent.class) {
             @Override
-            public void invoke(KeyboardEvent event)
-            {
-                if (event.getEventState())
-                {
-                    for (Module module : moduleManager.getRegistered())
-                    {
-                        if (isModuleValid(module) && module.getBind().getKey() == event.getKey())
-                        {
+            public void invoke(KeyboardEvent event) {
+                if (event.getEventState()) {
+                    for (Module module : moduleManager.getRegistered()) {
+                        if (isModuleValid(module) && module.getBind().getKey() == event.getKey()) {
                             module.toggle();
                         }
                     }
-                }
-                else
-                {
+                } else {
                     onRelease(event.getKey());
                 }
             }
         });
-        this.listeners.add(new EventListener<GuiScreenEvent<?>>
-                (GuiScreenEvent.class, Integer.MIN_VALUE + 10)
-        {
+        this.listeners.add(new EventListener<GuiScreenEvent<?>>(GuiScreenEvent.class, Integer.MIN_VALUE + 10) {
             @Override
-            public void invoke(GuiScreenEvent<?> event)
-            {
-                if (event.isCancelled() || event.getScreen() == null)
-                {
+            public void invoke(GuiScreenEvent<?> event) {
+                if (event.isCancelled() || event.getScreen() == null) {
                     return;
                 }
 
-                for (Module module : moduleManager.getRegistered())
-                {
-                    if (isModuleValid(module) && KeyBoardUtil.isKeyDown(module.getBind()))
-                    {
-                        switch (module.getBindMode())
-                        {
+                for (Module module : moduleManager.getRegistered()) {
+                    if (isModuleValid(module) && KeyBoardUtil.isKeyDown(module.getBind())) {
+                        switch (module.getBindMode()) {
                             case Hold:
-                                module.toggle();
+                                module.disable();
                                 break;
                             case Disable:
                                 module.disable();
@@ -74,14 +57,10 @@ public class KeyBoardManager extends SubscriberImpl
         });
     }
 
-    private void onRelease(int keyCode)
-    {
-        for (Module module : moduleManager.getRegistered())
-        {
-            if (isModuleValid(module) && module.getBind().getKey() == keyCode)
-            {
-                switch (module.getBindMode())
-                {
+    private void onRelease(int keyCode) {
+        for (Module module : moduleManager.getRegistered()) {
+            if (isModuleValid(module) && module.getBind().getKey() == keyCode) {
+                switch (module.getBindMode()) {
                     case Hold:
                         module.toggle();
                         break;
@@ -94,8 +73,7 @@ public class KeyBoardManager extends SubscriberImpl
         }
     }
 
-    protected boolean isModuleValid(Module module)
-    {
+    protected boolean isModuleValid(Module module) {
         return !PingBypass.isConnected();
     }
 
